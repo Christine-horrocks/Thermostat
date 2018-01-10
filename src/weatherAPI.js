41b1966028238a51f2832403aff1e345b9f68656
Christine <splogy2001@hotmail.com>
2018-01-10 13:16:18 +0000

    API Refactored

--- src/weatherAPI.js
+++ src/weatherAPI.js
@@ -1,19 +1,19 @@
 $(document).ready(function(){
 
-  $('#select-city').submit(function(event) {
-  event.preventDefault();
-  var city = $('#current-city').val();
-    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=25c49e7c49f21b58fa679a044316c269&units=metric', function(data) {
+  function displayWeather(city) {
+    var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city;
+    var token = '&appid=25c49e7c49f21b58fa679a044316c269';
+    var units = '&units=metric';
+    $.get(url + token + units, function(data) {
       $('#current-temperature').text(data.main.temp);
-    });
-  });
-
-  $('#select-city').submit(function(event) {
-  event.preventDefault();
-  var city = $('#current-city').val();
-    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=25c49e7c49f21b58fa679a044316c269&units=metric', function(data) {
       $('#current-weather').text(data.weather[0].main);
     });
-  });
+  }
+
+    $('#select-city').submit(function(event) {
+      event.preventDefault();
+      var city = $('#current-city').val();
+      displayWeather(city);
+    });
 
 });
