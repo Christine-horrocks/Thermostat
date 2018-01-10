e488af9bac6a0d4d96ce25228928c9a261b2ef42
Christine <splogy2001@hotmail.com>
2018-01-10 12:42:09 +0000

    User inputs city to use API

--- src/weatherAPI.js
+++ src/weatherAPI.js
@@ -1,9 +1,19 @@
 $(document).ready(function(){
-  $.get('http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=25c49e7c49f21b58fa679a044316c269&units=metric', function(data) {
-    $('#current-temperature').text(data.main.temp);
+
+  $('#select-city').submit(function(event) {
+  event.preventDefault();
+  var city = $('#current-city').val();
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=25c49e7c49f21b58fa679a044316c269&units=metric', function(data) {
+      $('#current-temperature').text(data.main.temp);
+    });
   });
 
-  $.get('http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=25c49e7c49f21b58fa679a044316c269&units=metric', function(data) {
-    $('#current-weather').text(data.weather[0].main);
+  $('#select-city').submit(function(event) {
+  event.preventDefault();
+  var city = $('#current-city').val();
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=25c49e7c49f21b58fa679a044316c269&units=metric', function(data) {
+      $('#current-weather').text(data.weather[0].main);
+    });
   });
+
 });
